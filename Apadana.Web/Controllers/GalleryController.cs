7575bfa7ba09b09a0be41fdfdd1775ea7f0d6b6a
Mohammadreza Khahani <mr.khahani@gmail.com>
2017-01-11 17:15:59 +0330

    Add Gallery and Post pages

---
+++ Apadana.Web/Controllers/GalleryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Apadana.Web.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallary
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListForSiteFooter()
+        {
+            return PartialView("_ListForSiteFooter");
+        }
+    }
+}
---
+++ Apadana.Web/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Apadana.Web.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Check(int id)
+        {
+            return View();
+        }
+        public ActionResult SimpleListForRightMenu()
+        {
+            return PartialView("_SimpleListForRightMenu");
+        }
+    }
+}
