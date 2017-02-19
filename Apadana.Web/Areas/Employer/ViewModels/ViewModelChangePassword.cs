af2044dec63f9b41e19d799a243c084b344af789
Mohammadreza Khahani <mr.khahani@gmail.com>
2017-02-19 17:28:28 +0330

    add validation and resources for change password

--- Apadana.Web/Areas/Employer/ViewModels/ViewModelChangePassword.cs
+++ Apadana.Web/Areas/Employer/ViewModels/ViewModelChangePassword.cs
@@ -1,23 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Apadana.Web.Areas.Employer.Localization;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace Apadana.Web.Areas.Employer.ViewModels
 {
     public class ViewModelChangePassword
     {
-        [Required(ErrorMessage ="رمز فعلی را وارد نمایید", AllowEmptyStrings =false)]
+        [Required(ErrorMessageResourceType =typeof(Resources), ErrorMessageResourceName = "Val_this_field_is_required", AllowEmptyStrings =false)]
         [DataType(DataType.Password)]
-        [Display(Name ="رمز فعلی")]
+        [Display(ResourceType = typeof(Resources), Name = "DisName_CurrentPassword")]
         public string CurrentPassword { get; set; }
-        [Required]
+
+        [Required(ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "Val_this_field_is_required", AllowEmptyStrings = false)]
         [DataType(DataType.Password)]
+        [Display(ResourceType = typeof(Resources), Name = "DisName_NewPassword")]
         public string NewPassword { get; set; }
-        [Required]
+
+        [Required(ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "Val_this_field_is_required", AllowEmptyStrings = false)]
         [DataType(DataType.Password)]
-        [Compare("NewPassword")]
+        [Display(ResourceType = typeof(Resources), Name = "DisName_ConfirmPassword")]
+        [Compare("NewPassword", ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "Val_confirm_is_not_the_same")]
         public string Confirm { get; set; }
     }
 }
